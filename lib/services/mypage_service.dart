c1479820ed21e3093a8f6c999aa844699f80325a
WithJo <researcherDragon@gmail.com>
2024-09-22 11:03:54 +0900

    chore: Add member data

--- lib/services/mypage_service.dart
+++ lib/services/mypage_service.dart
@@ -7,13 +7,14 @@ import 'package:pop_up_the_local/models/member_detail_model.dart';
 Future<dynamic> getMemberDetail() async {
   String? baseUrl = dotenv.env['BASE_URL'];
 
-  final response = await http.get(Uri.parse('$baseUrl/api/members'));
-  if (response.statusCode == 200) {
-    final Map<String, dynamic> data =
-        jsonDecode(utf8.decode(response.bodyBytes))['data'];
-    print(data);
-    return MemberDetail.fromJson(data);
-  } else {
-    throw Exception('Failed to load member detail');
-  }
+  //final response = await http.get(Uri.parse('$baseUrl/api/members'));
+
+  final Map<String, dynamic> data = {
+    'email': 'popup@example.com',
+    'role': 'SELLER',
+    'image': 'https://avatars.githubusercontent.com/u/86557146?v=4',
+    'name': '팝업더로컬',
+  };
+  print(data);
+  return MemberDetail.fromJson(data);
 }
